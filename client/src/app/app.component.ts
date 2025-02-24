9340cd7841a1a5f24ca4ccefce65c8838eb71b3d
Niranjan <52606175+NiranjanWaghmare@users.noreply.github.com>
2025-02-24 23:48:26 +0530

    Routing & UI theme - section 6

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, inject, OnInit } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet } from '@angular/router';
 import { NavComponent } from './nav/nav.component';
 import { AccountService } from './_services/account.service';
 import { HomeComponent } from './home/home.component';
@@ -7,7 +7,7 @@ import { HomeComponent } from './home/home.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [NavComponent, HomeComponent],
+  imports: [NavComponent, HomeComponent,RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -2,37 +2,38 @@ import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AccountService } from '../_services/account.service';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
-
-import { NgIf } from '@angular/common';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { TitleCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [FormsModule,BsDropdownModule],
+  imports: [FormsModule,BsDropdownModule,RouterLink,RouterLinkActive,TitleCasePipe],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.css'
 })
 export class NavComponent {
   model: any = {};
   accountService = inject(AccountService);
+  private router = inject(Router);
+  private toastr = inject(ToastrService);
 
 
   login(){
     this.model.username='cena'
     this.model.password = 'password'
     this.accountService.login(this.model).subscribe({
-      next: response => {
-        console.log(response);
-
+      next: _ => {
+        this.router.navigateByUrl('/members')
       },
-      error: errorResponse=>{
-        console.log(errorResponse.error);
-      }
+      error: errorResponse=> this.toastr.error(errorResponse.error)
     })
     
   }
 
   logout(){
     this.accountService.logout();
+    this.router.navigateByUrl('/');
   }
 }
